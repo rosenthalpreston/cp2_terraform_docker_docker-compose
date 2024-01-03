141c53992e7cac3520f8fbb89cfd43fd556ea738
rosenthalpreston <129431344+rosenthalpreston@users.noreply.github.com>
2024-01-03 11:06:00 +0100

    Create terraform.sh

---
+++ terraform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Importation de la clé GPG de HashiCorp
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+# Ajout du dépôt HashiCorp aux sources APT
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Mise à jour des paquets et installation de Terraform
+sudo apt update
+sudo apt install terraform
